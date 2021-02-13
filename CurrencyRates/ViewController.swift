0e1522a14c29c4f2ce0f8cf68c68b81bb763afd3
Alexander Maklakov <richmak@ya.ru>
2021-02-13 09:55:50 +0300

    Initial Commit

---
+++ CurrencyRates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyRates
+//
+//  Created by Alexander on 13.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
