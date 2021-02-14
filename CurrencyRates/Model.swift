ab37c77db457af562b6decde8c3c28ecf1a65fb4
Alexander Maklakov <richmak@ya.ru>
2021-02-14 12:24:55 +0300

    XMLparser added

--- CurrencyRates/Model.swift
+++ CurrencyRates/Model.swift
@@ -7,9 +7,6 @@
 
 import UIKit
 
-/*
- <NumCode>036</NumCode><CharCode>AUD</CharCode><Nominal>1</Nominal><Name>¿‚ÒÚ‡ÎËÈÒÍËÈ ‰ÓÎÎ‡</Name><Value>57,1181</Value>
- */
 class Currency {
     var numCode: String?
     var charCode: String?
@@ -23,17 +20,22 @@ class Currency {
     var valueDouble: Double?
 }
 
-class Model: NSObject {
+class Model: NSObject, XMLParserDelegate {
     static let shared = Model()
     
     var currencies: [Currency] = []
     
     var pathForXML: String {
-        return ""
+        let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.libraryDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]+"/data.xml"
+        if FileManager.default.fileExists(atPath: path) {
+            return path
+        }
+        
+        return Bundle.main.path(forResource: "data", ofType: "xml")!
     }
     
-    var urlForXML: URL? {
-        return nil
+    var urlForXML: URL {
+        return URL(fileURLWithPath: pathForXML)
     }
     
     //Загрузка XML с cbr.ru и сохранение его в каталоге приложения
@@ -43,7 +45,47 @@ class Model: NSObject {
     
     //Парсим XML и помещаем его в currencies, отправляем уведомление приложению о том, что данные обновились
     func parseXML() {
+        currencies = []
+        let parser = XMLParser(contentsOf: urlForXML)
+        parser?.delegate = self
+        parser?.parse()
+        print(currencies)
+    }
+    
+    var currentCurrency: Currency?
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        if elementName == "Valute" {
+            currentCurrency = Currency()
+        }
+    }
+    
+    var currentCharacters: String = ""
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        currentCharacters = string
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "NumCode" {
+            currentCurrency?.numCode = currentCharacters
+        }
+        if elementName == "CharCode" {
+            currentCurrency?.charCode = currentCharacters
+        }
+        if elementName == "Nominal" {
+            currentCurrency?.nominal = currentCharacters
+            currentCurrency?.nominalDouble = Double(currentCharacters.replacingOccurrences(of: ",", with: "."))
+        }
+        if elementName == "Name" {
+            currentCurrency?.name = currentCharacters
+        }
+        if elementName == "Value" {
+            currentCurrency?.value = currentCharacters
+            currentCurrency?.valueDouble = Double(currentCharacters.replacingOccurrences(of: ",", with: "."))
+        }
         
+        if elementName == "Valute" {
+            currencies.append(currentCurrency!)
+        }
     }
     
 }
